e29fdaab57b5d9d78182ba6e677b8123df312331
CODEZPC <zpcpy@outlook.com>
2025-01-29 17:28:39 +0800

    Undying效果 成功提交

--- src/main/java/com/code/Codeeffects/codeeffects.java
+++ src/main/java/com/code/Codeeffects/codeeffects.java
@@ -10,7 +10,6 @@ entity.removeStatusEffect(undyingStatusEffectEntry);
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
-// import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -24,6 +23,7 @@ class UndyingStatusEffect extends StatusEffect {
         entity.setHealth(20.0F);
         return true;
     }
+    @Override
     public boolean canApplyUpdateEffect(int duration, int amplifier) {
         return true;
     }
