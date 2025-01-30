349bb56e4998e0ba183308e5f7990e99c63392c0
CODEZPC <zpcpy@outlook.com>
2025-01-30 19:24:21 +0800

    OK?

---
+++ src/main/java/com/code/Codepotions/Codepotions.java
@@ -0,0 +1,20 @@
+package com.code.Codepotions;
+
+import net.fabricmc.fabric.api.registry.FabricBrewingRecipeRegistryBuilder;
+import net.minecraft.item.Items;
+import net.minecraft.potion.Potions;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
+import net.minecraft.recipe.Ingredient;
+
+public class Codepotions {
+    public static void Registry_Potions() {
+        FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
+			builder.registerPotionRecipe(
+					Potions.WATER,
+					Ingredient.ofItems(Items.IRON_SWORD),
+					Registries.POTION.getEntry(Identifier.of("code:blood_cloth")).get()
+			);
+		});
+    }
+}
