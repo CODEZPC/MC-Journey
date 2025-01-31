cfddc9e294a727086252f787d254fa2d21ab5df1
CODEZPC <zpcpy@outlook.com>
2025-01-31 17:42:42 +0800

    Blood cloth多实体

--- src/main/java/com/code/Codeeffects/codeeffects.java
+++ src/main/java/com/code/Codeeffects/codeeffects.java
@@ -54,27 +54,19 @@ class Undying extends StatusEffect {
 
 class BloodCloth extends StatusEffect {
     private int ticks = 0;
-    public static final Logger L= LoggerFactory.getLogger("bloodcloth");
     public BloodCloth() {
         super(StatusEffectCategory.BENEFICIAL, 0x3B0402);
     }
     @Override
     public boolean applyUpdateEffect(LivingEntity entity, int amplifier) {
         ticks++;
-        if (ticks >= 120 / (amplifier + 1) && entity.getHealth() > 5.0F) {
-            entity.setHealth(entity.getHealth() - 1.0F);
+        if (ticks >= 120 / (amplifier + 1) && entity.getHealth() > entity.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH).getBaseValue() / 4) {
+            entity.setHealth(entity.getHealth() - entity.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH).getBaseValue() / 20 * (amplifier + 1));
             ticks = 0;
         }
-        EntityAttributeInstance speed = entity.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED);
-        if (speed != null) {
-            int examp = (int) (20.0F - entity.getHealth()) / 5;
-            entity.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 5, amplifier + (amplifier + 1) * examp, true, false, false));
-        }
-        EntityAttributeInstance strength = entity.getAttributeInstance(EntityAttributes.GENERIC_ATTACK_DAMAGE);
-        if (strength != null) {
-            int examp = (int) (20.0F - entity.getHealth()) / 5;
-            entity.addStatusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 5, amplifier + (amplifier + 1) * examp, true, false, false));
-        }
+        int examp = (int) (entity.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH).getBaseValue() - entity.getHealth()) / (entity.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH).getBaseValue() / 4);
+        entity.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 5, amplifier + (amplifier + 1) * examp, true, false, false));
+        entity.addStatusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 5, amplifier + (amplifier + 1) * examp, true, false, false));
         return true;
     }
     @Override
