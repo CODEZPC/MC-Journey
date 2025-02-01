b08a8b89081a29c46257b4939269f72fee8cfd90
CODEZPC <zpcpy@outlook.com>
2025-02-01 15:27:06 +0800

    Format

--- src/main/java/com/code/MCJourney.java
+++ src/main/java/com/code/MCJourney.java
@@ -1,16 +1,16 @@
 /*
-
-* MC Journey for minecraft 1.21-1.21.1 by CODEZPC
-
-! 这其中的物品注册在 1.21.2 后进行了更改，需要大范围修改
-
-*/
-//包名称
-//Package name
+ * 
+ * MC Journey for minecraft 1.21-1.21.1 by CODEZPC
+ * 
+ * ! 这其中的物品注册在 1.21.2 后进行了更改，需要大范围修改
+ * 
+ */
+// 包名称
+// Package name
 package com.code;
 
-//基本包导入
-//Basic package import
+// 基本包导入
+// Basic package import
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,13 +21,14 @@ import com.code.Codepotions.codepotions;
 
 import net.fabricmc.api.ModInitializer;
 
-//主函数
-//Main function
+// 主函数
+// Main function
 public class MCJourney implements ModInitializer {
-    //定义日志输出器
-    //Define log outputter
+    // 定义日志输出器
+    // Define log outputter
     public static final Logger LOGGER = LoggerFactory.getLogger("code");
-    //LOGGER.xxx("xxx");
+
+    // LOGGER.xxx("xxx");
     @Override
     public void onInitialize() {
         codeblocks.Registry_Blocks();
