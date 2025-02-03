3ce1a121a60dfc04264b68066a05eb6ba144aa2e
CODEZPC <zpcpy@outlook.com>
2025-02-03 11:37:43 +0800

    RC

---
+++ src/main/java/com/code/Codeitems/items/Dot.java
@@ -0,0 +1,30 @@
+package com.code.Codeitems.items;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class Dot extends Item{
+    public Dot(Settings settings) {
+        super(settings);
+    }
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack stack = user.getStackInHand(hand);
+        if (stack.getCount() < 10) {
+            return TypedActionResult.fail(stack);
+        }
+        stack.decrement(10);
+        ItemStack diamondStack = new ItemStack(Items.DIAMOND);
+        PlayerInventory inventory = user.getInventory();
+        if (!inventory.insertStack(diamondStack)) {
+            user.dropItem(diamondStack, false);
+        }
+        return TypedActionResult.success(user.getStackInHand(hand));
+    }
+}
