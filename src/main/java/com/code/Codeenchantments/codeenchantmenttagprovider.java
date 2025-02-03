da1be6239368204078190db4d13ebd2338a8d483
CODEZPC <zpcpy@outlook.com>
2025-02-03 15:32:10 +0800

    FIX

--- src/main/java/com/code/Codeenchantments/codeenchantmenttagprovider.java
+++ src/main/java/com/code/Codeenchantments/codeenchantmenttagprovider.java
@@ -1,17 +1,24 @@
 package com.code.Codeenchantments;
 
+import net.fabricmc.api.ModInitializer;
 import net.minecraft.data.DataOutput;
 import java.util.concurrent.CompletableFuture;
 import net.minecraft.data.server.tag.EnchantmentTagProvider;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.EnchantmentTags;
 
-public class codeenchantmenttagprovider extends EnchantmentTagProvider{
+public class codeenchantmenttagprovider extends EnchantmentTagProvider implements ModInitializer {
     public codeenchantmenttagprovider(DataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
-		super(dataOutput, completableFuture);
-	}
+        super(dataOutput, completableFuture);
+    }
+
     @Override
     protected void configure(RegistryWrapper.WrapperLookup lookup) {
         this.getOrCreateTagBuilder(EnchantmentTags.CURSE).add(codeenchantments.EASY_BREAKING);
+    }
+
+    @Override
+    public void onInitialize() {
+        // Nothing
     }
 }
