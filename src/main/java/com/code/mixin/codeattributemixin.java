295f0988fdd48446546f7042913ca1a20bc31401
CODEZPC <zpcpy@outlook.com>
2025-03-01 17:52:20 +0800

    mixin

--- src/main/java/com/code/mixin/codeattributemixin.java
+++
@@ -1,8 +0,0 @@
-package com.code.mixin;
-
-import org.spongepowered.asm.mixin.Mixin;
-import net.minecraft.entity.attribute.DefaultAttributeRegistry;
-
-@Mixin(DefaultAttributeRegistry.class)
-public abstract class codeattributemixin {
-}
